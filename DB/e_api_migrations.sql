7e23b97cb52f5da95119d9b20199b7f5b16d813d
UltronDev <ultrondev61@gmail.com>
2017-12-21 17:30:06 +0500

    Migration for Product and Review Added and Also DB files are here

---
+++ DB/e_api_migrations.sql
@@ -0,0 +1,4 @@
+INSERT INTO e_api.migrations (id, migration, batch) VALUES (1, '2014_10_12_000000_create_users_table', 1);
+INSERT INTO e_api.migrations (id, migration, batch) VALUES (2, '2014_10_12_100000_create_password_resets_table', 1);
+INSERT INTO e_api.migrations (id, migration, batch) VALUES (3, '2017_12_21_114023_create_products_table', 1);
+INSERT INTO e_api.migrations (id, migration, batch) VALUES (4, '2017_12_21_114121_create_reviews_table', 1);
